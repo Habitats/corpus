91fdaf7f6ee71163886182aa43a490e59cc6a27d
Patrick Skjennum <mail@habitats.no>
2016-05-12 19:17:13 +0200

    conditionals in gradle

--- ai/install.sh
+++ ai/install.sh
@@ -20,10 +20,22 @@ git config --global user.email "mail@habitats.no"
 git config --global user.name "Patrick Skjennum"
 cd corpus && git remote set-url origin git@github.com:Habitats/corpus.git && cd ..
 
+# Maven
+if [ ! -f ~/apache-maven-3.3.3-bin.tar.gz ]; then
+	sudo apt-get purge -y maven
+	wget http://apache.cs.utah.edu/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
+	tar -zxf apache-maven-3.3.3-bin.tar.gz
+	sudo cp -R apache-maven-3.3.3 /usr/local
+	sudo ln -s /usr/local/apache-maven-3.3.3/bin/mvn /usr/bin/mvn
+	echo "export M2_HOME=/usr/local/apache-maven-3.3.3" >> ~/.profile
+	source ~/.profile
+	echo "Maven is on version `mvn -v`"
+fi
+
 # Downlaod corpus
 cd ~
-rm corpus-archive.*
-if [ -f ~/corpus-archive.zip ]; then
+if [ ! -f corpus-archive.zip ]; then
+	#rm corpus-archive.*
 	echo "Downloading ..."
     wget --progress=bar:force:noscroll https://www.dropbox.com/s/n0750x98l0hsrhp/corpus-archive.zip
 fi
@@ -33,35 +45,58 @@ if [ -d data ]; then
 	mv models ~/corpus/
 fi
 
+# cmake
+if [ ! -f cmake-3.2.2.tar.gz ]; then
+	sudo apt-get install build-essential
+	wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
+	tar xf cmake-3.2.2.tar.gz
+	cd cmake-3.2.2
+	./configure
+	make
+	sudo make install
+	echo "export PATH=/usr/local/bin:$PATH" >> ~/.profile
+	echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.profile
+	source ~./profile
+	cmake --version # This should give v3.2
+fi
+
 # DL4J
 mkdir ~/dl4j
 cd ~/dl4j
-yes | git clone git@github.com:deeplearning4j/libnd4j.git
+git clone https://github.com/deeplearning4j/libnd4j.git
+git pull
 cd libnd4j
 bash buildnativeoperations.sh cpu 
 #bash buildnativeoperations.sh -c cuda
-export LIBND4J_HOME=`pwd`
+echo "export LIBND4J_HOME=`pwd`" >> ~/.profile
+source ~./profile
 cd ..
-yes | git clone git@github.com:deeplearning4j/nd4j.git
+git clone https://github.com/deeplearning4j/nd4j.git
+git pull
 cd nd4j
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -pl '!:nd4j-cuda-7.5,!org.nd4j:nd4j-tests'
 cd ..
-git clone git@github.com:deeplearning4j/deeplearning4j.git
+git clone https://github.com/deeplearning4j/deeplearning4j.git
+git pull
 cd deeplearning4j
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true
 
 # Gradle
-mkdir -p ~/opt/packages/gradle 
-cd $_ 
-wget --progress=bar:force:noscroll https://services.gradle.org/distributions/gradle-2.10-bin.zip 
-unzip -o gradle-2.10-bin.zip 
-ln -s ~/opt/packages/gradle/gradle-2.10/ ~/opt/gradle 
-echo "if [ -d \"\$HOME/opt/gradle\" ]; then
-    export GRADLE_HOME=\"\$HOME/opt/gradle\"
-    PATH=\"\$PATH:\$GRADLE_HOME/bin\"
-fi" >> ~/.profile
-source ~/.profile
-source ~/.bashrc
+
+cd ~
+if [ -d opt ]; then
+	mkdir -p ~/opt/packages/gradle 
+	cd $_ 
+	wget --progress=bar:force:noscroll https://services.gradle.org/distributions/gradle-2.10-bin.zip 
+	unzip -o gradle-2.10-bin.zip 
+	ln -s ~/opt/packages/gradle/gradle-2.10/ ~/opt/gradle 
+	echo "if [ -d \"\$HOME/opt/gradle\" ]; then
+	    export GRADLE_HOME=\"\$HOME/opt/gradle\"
+	    PATH=\"\$PATH:\$GRADLE_HOME/bin\"
+	fi" >> ~/.profile
+	source ~/.profile
+	source ~/.bashrc
+fi
 
 cd ~/corpus/ 
 ~/opt/gradle/bin/gradle clean jar shadowJar
