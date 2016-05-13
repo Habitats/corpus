c89eebebc0c65662a4ece52739fcf5a07c95e4a3
Patrick Skjennum <mail@habitats.no>
2016-05-13 14:11:16 +0200

    use broadcasting for spark

--- common/src/main/scala/no/habitats/corpus/common/W2VLoader.scala
+++ common/src/main/scala/no/habitats/corpus/common/W2VLoader.scala
@@ -3,9 +3,9 @@ package no.habitats.corpus.common
 import java.io.{File, FileNotFoundException}
 
 import no.habitats.corpus.common.CorpusContext.sc
-import no.habitats.corpus.common.W2VLoader._
 import no.habitats.corpus.common.dl4j.FreebaseW2V
 import no.habitats.corpus.common.models.{Annotation, Article}
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 import org.json4s.NoTypeHints
 import org.json4s.jackson.Serialization
@@ -20,21 +20,45 @@ import scala.collection.{Map, Set}
 //  lazy val ids            : Set[String]           = Config.dataFile(Config.freebaseToWord2VecIDs).getLines().toSet
 //}
 
-trait VectorLoader {
+sealed trait VectorLoader {
+
+  def loadVectors(vectorFile: String, filter: Set[String] = Set.empty): Map[String, INDArray] = {
+    Log.v(s"Loading cached W2V vectors ($vectorFile) ...")
+    if (!new File(vectorFile).exists) throw new FileNotFoundException(s"No cached vectors: $vectorFile")
+    val start = System.currentTimeMillis
+    var vec = sc.textFile("file:///" + vectorFile)
+      .map(_.split(","))
+      .filter(arr => filter.isEmpty || filter.contains(arr(0)))
+      .map(arr => (arr(0), arr.toSeq.slice(1, arr.length).map(_.toFloat).toArray))
+      .map(arr => {
+        val vector = Nd4j.create(arr._2)
+        val id = arr._1.trim
+        (id, vector)
+      }).collect.toMap
+
+    // SHOULD ALWAYS BE EQUAL!
+    val s = vec.size
+    vec = vec.filter(_._2.size(1) == 1000)
+    if (s != vec.size) throw new IllegalStateException(s"Vector filtering went wrong. Should be $s, was ${vec.size}!")
+
+    Log.v(s"Loaded vectors in ${System.currentTimeMillis() - start} ms")
+    vec
+  }
+
   def fromId(fb: String): Option[INDArray]
   def contains(fb: String): Boolean
   def documentVector(a: Article): INDArray
   def preload(): Unit
 }
 
-trait BinaryVectorLoader extends VectorLoader {
+private class BinaryVectorLoader extends VectorLoader {
   override def fromId(fb: String): Option[INDArray] = if (contains(fb)) Some(Nd4j.create(FreebaseW2V.gVec.getWordVector(fb))) else None
   override def documentVector(a: Article): INDArray = W2VLoader.calculateDocumentVector(a.ann)
   override def contains(fb: String): Boolean = FreebaseW2V.gVec.hasWord(fb)
   override def preload(): Unit = FreebaseW2V.gVec
 }
 
-trait TextVectorLoader extends VectorLoader {
+private class TextVectorLoader extends VectorLoader {
   lazy val vectors        : Map[String, INDArray] = loadVectors(Config.freebaseToWord2Vec(W2VLoader.confidence))
   lazy val documentVectors: Map[String, INDArray] = loadVectors(Config.documentVectors(W2VLoader.confidence))
   lazy val ids            : Set[String]           = Config.dataFile(Config.freebaseToWord2VecIDs).getLines().toSet
@@ -42,20 +66,44 @@ trait TextVectorLoader extends VectorLoader {
   override def fromId(fb: String): Option[INDArray] = vectors.get(fb)
   override def documentVector(a: Article): INDArray = {
     W2VLoader.calculateDocumentVector(a.ann)
-//    documentVectors.getOrElse(a.id, W2VLoader.calculateDocumentVector(a.ann))
+    documentVectors.getOrElse(a.id, W2VLoader.calculateDocumentVector(a.ann))
   }
   override def contains(fb: String): Boolean = ids.contains(fb)
   override def preload(): Unit = vectors
 }
 
-object W2VLoader extends RddSerializer with TextVectorLoader {
+sealed class SparkVectorLoader extends VectorLoader {
+
+  // Spark broadcasting (it's not pretty but maybe it works?)
+  var bVectors        : Broadcast[Map[String, INDArray]] = null
+  var bDocumentVectors: Broadcast[Map[String, INDArray]] = null
+
+  override def preload(): Unit = {
+    bVectors = CorpusContext.sc.broadcast[Map[String, INDArray]](loadVectors(Config.freebaseToWord2Vec(W2VLoader.confidence)))
+    bDocumentVectors = CorpusContext.sc.broadcast[Map[String, INDArray]](loadVectors(Config.documentVectors(W2VLoader.confidence)))
+  }
+
+  override def contains(fb: String): Boolean = bVectors.value.contains(fb)
+  override def fromId(fb: String): Option[INDArray] = bVectors.value.get(fb)
+  override def documentVector(a: Article): INDArray = bDocumentVectors.value.getOrElse(a.id, W2VLoader.calculateDocumentVector(a.ann))
+}
+
+object W2VLoader extends RddSerializer with VectorLoader {
 
   implicit val formats = Serialization.formats(NoTypeHints)
+  val loader: VectorLoader = if (Config.local) new SparkVectorLoader() else new TextVectorLoader()
 
   // TODO: NOT GOOD
-  var confidence = 0.5
-
+  var confidence  = 0.5
   val featureSize = 1000
+
+  def preload(): Unit = loader.preload()
+
+  def fromId(fb: String): Option[INDArray] = loader.fromId(fb)
+
+  def contains(fb: String): Boolean = loader.contains(fb)
+
+  def documentVector(a: Article): INDArray = loader.documentVector(a)
 
   def calculateDocumentVector(ann: Map[String, Annotation]): INDArray = {
     val vectors: Iterable[INDArray] = ann.values.map(_.fb).flatMap(fromId)
@@ -85,26 +133,4 @@ object W2VLoader extends RddSerializer with TextVectorLoader {
   def toString(w2v: INDArray): String = w2v.data().asFloat().mkString(",")
   def fromString(w2v: String): INDArray = Nd4j.create(w2v.split(",").map(_.toFloat))
 
-  def loadVectors(vectorFile: String, filter: Set[String] = Set.empty): Map[String, INDArray] = {
-    Log.v(s"Loading cached W2V vectors ($vectorFile) ...")
-    if (!new File(vectorFile).exists) throw new FileNotFoundException(s"No cached vectors: $vectorFile")
-    val start = System.currentTimeMillis
-    var vec = sc.textFile("file:///" + vectorFile)
-      .map(_.split(","))
-      .filter(arr => filter.isEmpty || filter.contains(arr(0)))
-      .map(arr => (arr(0), arr.toSeq.slice(1, arr.length).map(_.toFloat).toArray))
-      .map(arr => {
-        val vector = Nd4j.create(arr._2)
-        val id = arr._1.trim
-        (id, vector)
-      }).collect.toMap
-
-    // SHOULD ALWAYS BE EQUAL!
-    val s = vec.size
-    vec = vec.filter(_._2.size(1) == 1000)
-    if (s != vec.size) throw new IllegalStateException(s"Vector filtering went wrong. Should be $s, was ${vec.size}!")
-
-    Log.v(s"Loaded vectors in ${System.currentTimeMillis() - start} ms")
-    vec
-  }
 }
