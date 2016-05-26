04e7d633b91af664a810ce815c99fb5ec12438f1
Patrick Skjennum <mail@habitats.no>
2016-05-26 02:17:09 +0200

    fix: instal scripts

---
+++ ai/addkey.sh
@@ -0,0 +1,27 @@
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus8-1.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus8-2.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus8-3.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus8-4.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus16-1.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus16-2.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus16-3.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus16-4.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus32-1.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus32-2.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus32-3.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine.pub ~/.ssh/google_compute_engine.pub habispam@corpus32-4.europe-west1-c.ntnu-smartmedia:~/.ssh/
+
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus8-1.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus8-2.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus8-3.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus8-4.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus16-1.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus16-2.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus16-3.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus16-4.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus32-1.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus32-2.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus32-3.europe-west1-c.ntnu-smartmedia:~/.ssh/
+scp -oStrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine habispam@corpus32-4.europe-west1-c.ntnu-smartmedia:~/.ssh/
--- ai/install_all.sh
+++ ai/install_all.sh
@@ -1,16 +1,17 @@
-sudo echo "deb http://http.us.debian.org/debian stable main contrib non-free" | sudo tee -a /etc/apt/sources.list # enable non-free packages
+#sudo echo "deb http://http.us.debian.org/debian stable main contrib non-free" | sudo tee -a /etc/apt/sources.list # enable non-free packages
 sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
 sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 sudo apt-get update
-sudo apt-get install -y htop unrar build-essential git unzip oracle-java8-installer
+sudo apt-get install -y htop build-essential git unzip oracle-java8-installer
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> ~/.profile && . ~/.profile
  
-sudo wget www.scala-lang.org/files/archive/scala-2.11.7.deb
-sudo dpkg -i scala-2.11.7.deb
+sudo wget www.scala-lang.org/files/archive/scala-2.11.7.deb && sudo dpkg -i scala-2.11.7.deb
 
-echo "alias submit='spark-submit --class no.habitats.corpus.spark.SparkUtil --jars ~/corpus/ai/build/libs/ai-all.jar ~/corpus/ai/build/libs/ai.jar '" >> ~/.bashrc
-echo "alias build='cd ~/corpus/ && git pull && gradle ai:clean ai:shadowJar'" >> ~/.bashrc
+echo "alias submit='screen -S spark spark-submit --class no.habitats.corpus.spark.SparkUtil --jars ~/corpus/ai-all.jar ~/corpus/ai/build/libs/ai.jar '" >> ~/.bashrc
+echo "alias submit2='spark-submit --class no.habitats.corpus.spark.SparkUtil --jars ~/corpus/ai-all.jar ~/corpus/ai/build/libs/ai.jar '">> ~/.bashrc
+echo "alias build='cd ~/corpus/ && git pull && gradle ai:clean ai:jar -PscalaBinary=2.11 -PscalaVersion=2.11.6'">> ~/.bashrc
+echo "alias buildfat='cd ~/corpus/ && git pull && gradle ai:clean ai:shadowJar ai:jar -PscalaBinary=2.11 -PscalaVersion=2.11.6 && mv -f ~/corpus/ai/build/libs/ai-all.jar ~/corpus/'">> ~/.bashrc
 
 # cmake
 cd ~ 
@@ -26,37 +27,6 @@ if [ ! -d cmake-3.2.2 ]; then
 	. ~./profile
 	cmake --version # This should give v3.2
 fi
-
-wget http://registrationcenter-download.intel.com/akdlm/irc_nas/9068/l_mkl_11.3.3.210.tgz
-tar xf l_mkl_11.3.3.210.tgz
-
-mkdir ~/data/ && cd ~/data
-wget --progress=bar:force:noscroll https://storage.googleapis.com/ntnu-corpus/fb_w2v_0.5.rar
-unrar e fb_w2v_0.5.rar && mkdir w2v && mv fb_*txt w2v/
-
-wget --progress=bar:force:noscroll https://storage.googleapis.com/ntnu-corpus/document_vectors_0.5.rar
-unrar e document_vectors_0.5.rar && mv doc*txt w2v/
-
-wget --progress=bar:force:noscroll https://storage.googleapis.com/ntnu-corpus/nyt.rar
-wget --progress=bar:force:noscroll https://dl.dropboxusercontent.com/u/30450949/nyt_sub.rar
-unrar e nyt.rar
-unrar e nyt_sub.rar
-mkdir nyt && mv *txt nyt/ 
-
-cd ~
-wget http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.4.tgz
-tar xvf spark-1.5.2-bin-hadoop2.4.tgz
-mv spark-1.5.2-bin-hadoop2.4 spark
-cd ~/spark/conf/
-touch spark-defaults.conf
-echo "spark.master                    local[32]" >> spark-defaults.conf
-echo "spark.driver.maxResultSize      0"  >> spark-defaults.conf
-echo "spark.eventLog.dir              /home/patricls/.cache"  >> spark-defaults.conf
-echo "spark.driver.memory             60g"  >> spark-defaults.conf
-echo "spark.executor.extraJavaOptions -XX:+UseConcMarkSweepGC"  >> spark-defaults.conf
-echo "spark.driver.extraJavaOptions   -XX:+UseConcMarkSweepGC"  >> spark-defaults.conf
-echo "export PATH=~/spark/bin:$PATH" >> ~/.profile
-
 
 # Gradle
 cd ~
@@ -95,7 +65,7 @@ if [ ! -d dl4j ]; then
 	git clone https://github.com/deeplearning4j/libnd4j.git 
 	git clone https://github.com/deeplearning4j/nd4j.git
 	git clone https://github.com/habitats/deeplearning4j.git
-	git clone https://github.com/deeplearning4j/Canova.git
+	git clone https://github.com/deeplearning4j/canova.git
 
 	cd ~/dl4j/libnd4j && bash buildnativeoperations.sh cpu && echo "export LIBND4J_HOME=`pwd`" >> ~/.profile && export LIBND4J_HOME=`pwd` && . ~/.profile
 	cd ~/dl4j/nd4j && /usr/local/apache-maven-3.3.3/bin/mvn clean install -DskipTests -Dmaven.javadoc.skip=true -pl '!:nd4j-cuda-7.5,!org.nd4j:nd4j-tests'
