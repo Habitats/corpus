88e637956d693879911bdeb7ec75e540162a9829
Patrick Skjennum <mail@habitats.no>
2016-05-29 20:45:08 +0200

    fix: avoid TFIDF lookup inside spark environment, as it crucifies the memory and kills kittens

--- ai/src/main/scala/no/habitats/corpus/spark/CorpusDataset.scala
+++ ai/src/main/scala/no/habitats/corpus/spark/CorpusDataset.scala
@@ -1,7 +1,7 @@
 package no.habitats.corpus.spark
 
 import no.habitats.corpus.common.models.Article
-import no.habitats.corpus.common.{IPTC, TFIDF, W2VLoader}
+import no.habitats.corpus.common.{CorpusContext, IPTC, TFIDF, W2VLoader}
 import no.habitats.corpus.mllib.MlLibUtils
 import org.apache.spark.rdd.RDD
 import org.deeplearning4j.spark.util.MLLibUtil
@@ -23,7 +23,7 @@ object CorpusDataset {
   def labelArray(article: Article): Array[Int] = IPTC.topCategories.map(i => if (article.iptc.contains(i)) 1 else 0).toArray
 
   def genW2VDataset(articles: RDD[Article]): CorpusVectors = {
-    CorpusVectors(articles.map(a => (W2VLoader.documentVector(a), labelArray(a))).collect)
+    CorpusVectors(articles.collect().map(a => (W2VLoader.documentVector(a), labelArray(a))))
   }
 
   def genW2VMatrix(articles: RDD[Article]): CorpusMatrix = {
@@ -34,6 +34,6 @@ object CorpusDataset {
   }
 
   def genBoWDataset(articles: RDD[Article], tfidf: TFIDF): CorpusVectors = {
-    CorpusVectors(articles.map(a => (MLLibUtil.toVector(MlLibUtils.toVector(Some(tfidf), a)), IPTC.topCategories.map(i => if (a.iptc.contains(i)) 1 else 0).toArray)).collect())
+    CorpusVectors(articles.collect().map(a => (MLLibUtil.toVector(MlLibUtils.toVector(Some(tfidf), a)), IPTC.topCategories.map(i => if (a.iptc.contains(i)) 1 else 0).toArray)))
   }
 }
