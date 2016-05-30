258e7b23be3221d4734c66cd9529bc53ad7ab8b5
Patrick Skjennum <mail@habitats.no>
2016-05-30 13:24:09 +0200

    fix: move collect to the end

--- ai/src/main/scala/no/habitats/corpus/spark/CorpusDataset.scala
+++ ai/src/main/scala/no/habitats/corpus/spark/CorpusDataset.scala
@@ -26,11 +26,11 @@ object CorpusDataset {
   def labelArray(article: Article): Array[Int] = IPTC.topCategories.map(i => if (article.iptc.contains(i)) 1 else 0).toArray
 
   def genW2VDataset(articles: RDD[Article]): CorpusVectors = {
-    CorpusVectors(articles.collect().map(a => (a.id, annotationSet(a), labelArray(a), 1000)))
+    CorpusVectors(articles.map(a => (a.id, annotationSet(a), labelArray(a), 1000)).collect())
   }
 
   def genBoWDataset(articles: RDD[Article], size: Int): CorpusVectors = {
-    CorpusVectors(articles.collect().map(a => (a.id, annotationSet(a), labelArray(a), size)))
+    CorpusVectors(articles.map(a => (a.id, annotationSet(a), labelArray(a), size)).collect())
   }
 
   def genW2VMatrix(articles: RDD[Article]): CorpusMatrix = {
