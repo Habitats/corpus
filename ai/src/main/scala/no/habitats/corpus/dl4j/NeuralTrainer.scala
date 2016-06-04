bd7f3450824586ed6e3ebe2c9f6545ff7ab6ffaa
Patrick Skjennum <mail@habitats.no>
2016-06-04 21:20:19 +0200

    fix: correct number of articles to run intermediate eval on

--- ai/src/main/scala/no/habitats/corpus/dl4j/NeuralTrainer.scala
+++ ai/src/main/scala/no/habitats/corpus/dl4j/NeuralTrainer.scala
@@ -32,9 +32,10 @@ object NeuralTrainer {
       var c = 1
       while (trainIter.hasNext) {
         net.fit(trainIter.next())
-        if ((((c - 1) * trainIter.batch) % 10000) == 0) {
+        val intermediateFrequency: Int = 10000
+        if ((((c - 1) * trainIter.batch) % intermediateFrequency) == 0) {
           val left = timeLeft(totalTrainingSize = total, currentIteration = c, batch = batch, label = label, currentEpoch = epoch, totalEpoch = totalEpochs)
-          NeuralEvaluation(testIter.asScala.take(2).toTraversable, net, epoch, label, Some(neuralPrefs), Some(left)).log(resultFile, c - 1)
+          NeuralEvaluation(testIter.asScala.take(intermediateFrequency / batch).toTraversable, net, epoch, label, Some(neuralPrefs), Some(left)).log(resultFile, c - 1)
           testIter.reset()
         }
         c += 1
